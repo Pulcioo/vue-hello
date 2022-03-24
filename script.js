0695e4034113ab03d6813de6e5d94135d12ed560
Davide Pulci <davide.pulci@gmail.com>
2022-03-24 16:53:25 +0100

    fine

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const app = new Vue(
     {
         el: '#vue-app',
         data: {
-            message: 'Vue Hello!'
+            message: 'Vue Hello!',
+            image: 'https://picsum.photos/200'
         }
     }
 );
